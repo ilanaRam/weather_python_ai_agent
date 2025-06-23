4fa04c8715410592a2f6b3c4aee9b93e281fd89a
ilana <ilanaaprilloriram@gmail.com>
2025-06-24 00:19:44 +0300

    adding voice class

--- src/weather_agent.py
+++ src/weather_agent.py
@@ -1,75 +1,86 @@
 import requests # for using API and by api GET to read a weather per city from the site
-import pyttsx3  # for voice answering
 from pathlib import Path
 import os
 import yaml
+from voice import Voice
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+class Weather_Agent:
+    def __init__(self):
+        self.base_url = None
+        self.api_key = None
+        self.init()
 
-def speak(text, if_to_speak = False):
-    if if_to_speak:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
+    def init(self):
+        configs_file = self._find_full_file_path(Path.cwd().parent, "configs.yaml")
+        if not configs_file:
+            raise FileExistsError
+        print(f"\nLoading configs file from: {configs_file}")
+        with open(configs_file, "r") as configs_yaml_file:
+            content = yaml.safe_load(configs_yaml_file)
+            self.base_url = content["BASE_URL"]
+            print(f"Base URL is: {self.base_url}")
 
-def get_weather_params_by_city(city_name, key):
-    params = {
-        "q": city_name,
-        "appid": key,
-        "units": "metric"
-    }
-    return params
+        key_file = self._find_full_file_path(Path.cwd().parent, "key.yaml")
+        if not key_file:
+            raise FileExistsError
+        print(f"\nLoading key file from: {key_file}\n")
+        with open(key_file, "r") as key_yaml_file:
+            content = yaml.safe_load(key_yaml_file)
+            self.api_key = content["API_KEY"]
+            print(f"API_Key is: {self.api_key}")
 
-def get_api_key(key_file):
-    with open(key_file, "r") as key_yaml_file:
-        content = yaml.safe_load(key_yaml_file)
-        key = content["API_KEY"]
-        print(f"API_Key is: {key}")
-    return key
+    def _get_weather_params_by_city(self, city_name):
+        params = {
+            "q": city_name,
+            "appid": self.api_key,
+            "units": "metric"
+        }
+        return params
 
-def _find_full_file_path(my_curr_path, desired_file):
-    for dirpath, _, filenames in os.walk(my_curr_path):  #
-        if desired_file in filenames:
-            full_file_path = Path(str(os.path.join(dirpath, desired_file)))  # Return full path if found
-            return full_file_path
-    return None  # Return None if not found
+    def _find_full_file_path(self, my_curr_path, desired_file):
+        for dirpath, _, filenames in os.walk(my_curr_path):  #
+            if desired_file in filenames:
+                full_file_path = Path(str(os.path.join(dirpath, desired_file)))  # Return full path if found
+                return full_file_path
+        return None  # Return None if not found
+
+    def get_weather(self,desired_city, voice_obj):
+        params = self._get_weather_params_by_city(city_name=desired_city)
+        # here I use simply the REST API: GET to request a data from a site, using api this site supplies
+        response = requests.get(self.base_url,
+                                params=params)
+        if response.status_code == 200:
+            data = response.json()
+            temperature = data["main"]["temp"]
+            description = data["weather"][0]["description"]
+
+            text = f"\n🌤️ Weather in {desired_city}:"  # this Sun behind clouds is emojy: \U0001F324
+            print(text)
+            voice_obj.make_voice(text)
+
+            text = f"   Temperature: {temperature}°C"
+            print(text)
+            voice_obj.make_voice(text)
+
+            text = f"   Description: {description}"
+            print(text)
+            voice_obj.make_voice(text)
+        else:
+            text = "\n❌ Couldn't find the city. Please check the name and try again."
+            print(text)
+            voice_obj.make_voice("\n❌ Couldn't find the city. Please check the name and try again.")
+
+
 
 def main():
-    speak("Enter the city",True)
+    voice_obj = Voice()
+    weather_obj = Weather_Agent()
+
+    voice_obj.set_voice(True)
+    voice_obj.make_voice(my_text="Enter the city")
     city = input("Enter a city name: ")
+    weather_obj.get_weather(city, voice_obj)
 
-    key_file = _find_full_file_path(Path.cwd().parent, "key.yaml")
-    if not key_file:
-        raise FileExistsError
-    print(f"Loading key file from: {key_file}")
-
-    key = get_api_key(key_file)
-    params = get_weather_params_by_city(city,key)
-
-    # here I use simply the REST API: GET to request a data from a site, using api this site supplies
-    response = requests.get(BASE_URL,
-                            params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        temperature = data["main"]["temp"]
-        description = data["weather"][0]["description"]
-
-        text = f"\n🌤️ Weather in {city}:"  # this Sun behind clouds is emojy: \U0001F324
-        print(text)
-        speak(text, True)
-
-        text = f"   Temperature: {temperature}°C"
-        print(text)
-        speak(text)
-
-        text = f"   Description: {description}"
-        print(text)
-        speak(text)
-    else:
-        text = "\n❌ Couldn't find the city. Please check the name and try again."
-        print(text)
-        speak("\n❌ Couldn't find the city. Please check the name and try again.")
 
 if __name__ == "__main__":
     main()
